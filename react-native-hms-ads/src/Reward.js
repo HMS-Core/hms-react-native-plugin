ac426e38e2df5c0d1adb9d8b4c9e90d99e36b412
Aktug Urun <aktugurun@gmail.com>
2021-04-02 22:19:12 +0300

    UPDATE react-native-hms-ads 13.4.35-301 (#90)

--- react-native-hms-ads/src/Reward.js
+++ react-native-hms-ads/src/Reward.js
@@ -1,5 +1,5 @@
 /*
-    Copyright 2020. Huawei Technologies Co., Ltd. All rights reserved.
+    Copyright 2020-2021. Huawei Technologies Co., Ltd. All rights reserved.
 
     Licensed under the Apache License, Version 2.0 (the "License")
     you may not use this file except in compliance with the License.
@@ -14,22 +14,21 @@
     limitations under the License.
 */
 
-import {NativeModules} from "react-native";
+import {NativeModules, Platform} from "react-native";
 import {addListenerToModule} from "./utils";
 
 const {HMSAdsRewardAd} = NativeModules;
 
-const events = [
-  "adFailedToLoad",
-  "adLoaded",
-  "adClosed",
-  "adFailedToShow",
-  "adOpened",
-  "adRewarded",
-  "adLeftApp",
-  "adCompleted",
-  "adStarted",
-];
-addListenerToModule(HMSAdsRewardAd, events);
+if(Platform.OS == "android"){
+  const events = [
+    "adFailedToLoad",
+    "adLoaded",
+    "adClosed",
+    "adFailedToShow",
+    "adOpened",
+    "adRewarded",
+  ];
+  addListenerToModule(HMSAdsRewardAd, events);
+}
 
 export default HMSAdsRewardAd;
