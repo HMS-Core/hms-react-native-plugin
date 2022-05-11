d8d9079cc43a79b07525e56c79be4d867391db05
Bengisu <32999874+ozkulbeng@users.noreply.github.com>
2022-05-11 11:49:28 +0300

    UPDATE react-native-hms-push 6.3.0-304

--- react-native-hms-push/android/src/main/java/com/huawei/hms/rn/push/logger/HMSLogger.java
+++ react-native-hms-push/android/src/main/java/com/huawei/hms/rn/push/logger/HMSLogger.java
@@ -38,7 +38,7 @@ public final class HMSLogger {
 
     private static final String KIT = "Push"; 
     private static final String PLATFORM = "ReactNative"; 
-    private static final String VERSION = "6.3.0.302"; 
+    private static final String VERSION = "6.3.0.304"; 
 
     private static final String SERVICE = "Cross-Platform";
 
