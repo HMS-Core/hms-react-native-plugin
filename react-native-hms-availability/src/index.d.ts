84be4577ef57e5113d1bbaeb0e3f90f414378b74
Ryadi Sutjiptoputra <ryadisp@gmail.com>
2022-11-13 23:34:33 +0800

    fix: correction for types

--- react-native-hms-availability/src/index.d.ts
+++ react-native-hms-availability/src/index.d.ts
@@ -17,12 +17,12 @@ declare module "@hmscore/react-native-hms-availability" {
 
   
   export enum ErrorCode {
-    HMS_CORE_APK_AVAILABLE = "0",
-    NO_HMS_CORE_APK = "1",
-    HMS_CORE_APK_OUT_OF_DATE = "2",
-    HMS_CORE_APK_UNAVAILABLE = "3",
-    HMS_CORE_APK_IS_NOT_OFFICIAL_VERSION = "9",
-    HMS_CORE_APK_TOO_OLD = "21",
+    HMS_CORE_APK_AVAILABLE = 0,
+    NO_HMS_CORE_APK = 1,
+    HMS_CORE_APK_OUT_OF_DATE = 2,
+    HMS_CORE_APK_UNAVAILABLE = 3,
+    HMS_CORE_APK_IS_NOT_OFFICIAL_VERSION = 9,
+    HMS_CORE_APK_TOO_OLD = 21,
   }
 
   export interface ApiMap {
@@ -36,77 +36,77 @@ declare module "@hmscore/react-native-hms-availability" {
     'HuaweiPush.API': number
   }
   
-  export class HMSAvailability{
+  export default class HMSAvailability{
 
     /**
      * Obtains the API verssion number of each service.
      */
-    getApiMap(): Promise<ApiMap>;
+    static getApiMap(): Promise<ApiMap>;
 
     /**
      * Obtains the minimum version number of HMS Core that is supported currently.
      */
-    getServicesVersionCode(): Promise<number>;
+    static getServicesVersionCode(): Promise<number>;
 
     
     /**
      * Displays a readable text result code returned by the isHuaweiMobileServicesAvailable
      * (minApkVersion?: number) method.
      */
-    getErrorString(errorCode: number): Promise<string>;
+    static getErrorString(errorCode: number): Promise<string>;
 
     /**
      * Checks whether HMS Core (APK) is successfully installed and integrated on a device, 
      * and whether the version of the installed APK is that required by the client or is later 
      * than the required version.
      */
-    isHuaweiMobileServicesAvailable(minApkVersion?: number): Promise<number>;
+    static isHuaweiMobileServicesAvailable(minApkVersion?: number): Promise<number>;
 
     /**
      * Checks whether the HMS Core (APK) version supports notice obtaining.
      */
-    isHuaweiMobileNoticeAvailable(): Promise<number>;
+    static isHuaweiMobileNoticeAvailable(): Promise<number>;
 
     /**
      * Checks whether an exception is rectified through user operations.
      */
-    isUserResolvableError(errorCode: number): Promise<boolean>; 
+    static isUserResolvableError(errorCode: number): Promise<boolean>; 
 
     
     /**
      * Displays a notification or dialog box is displayed for the returned result code if
      * an exception can be rectified through user operations.
      */
-    resolveError(errorCode: number, requestCode: number): Promise<void>;
+    static resolveError(errorCode: number, requestCode: number): Promise<void>;
 
     
     /**
      * Sets the minimum version number of HMS Core that is supported currently.
      */
-    setServicesVersionCode(servicesVersionCode: number): Promise<void>;
+    static setServicesVersionCode(servicesVersionCode: number): Promise<void>;
     
     /**
      * Creates and displays a dialog box for a result code.
      */
-    showErrorDialogFragment(errorCode: number, requestCode: number): Promise<boolean>;
+    static showErrorDialogFragment(errorCode: number, requestCode: number): Promise<boolean>;
 
     
     /**
      * Creates and displays a dialog box for a result code.
      */
-    showErrorNotification(errorCode: number): Promise<void>;
+    static showErrorNotification(errorCode: number): Promise<void>;
 
     
     /**
      * Add a listener for the event when dialog fragment cancelled.
      */
-    OnErrorDialogFragmentCancelledListenerAdd(listenerFn: () => void): void;
+    static OnErrorDialogFragmentCancelledListenerAdd(listenerFn: () => void): void;
 
     
     /**
     * Remove the listener for the event when dialog fragment cancelled.
      */
-    OnErrorDialogFragmentCancelledListenerRemove(): void;
+    static OnErrorDialogFragmentCancelledListenerRemove(): void;
 
   }
 
